1e88d6e673bddb16bd2f274127342783034899f3
Alexander Platov <alexander.platov@hardcoreeng.com>
2025-07-05 02:52:32 +0700

    add SASS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
   import { onMount } from 'svelte'
   import { init, miniApp, postEvent } from '@telegram-apps/sdk-svelte'
 
+  let consEl: HTMLParagraphElement
+
   const initializeTelegramSDK = async () => {
     try {
       await init()
@@ -10,21 +12,30 @@
       if (miniApp.ready.isAvailable()) {
         await miniApp.ready()
         console.log('Mini App готово')
+        consEl.innerText = 'Mini App готово'
       }
 
 
     } catch (error) {
       console.error('Ошибка инициализации:', error)
+      consEl.innerText = 'Ошибка инициализации: ' + error
     }
   }
 
   onMount(() => {
     void initializeTelegramSDK()
 
-    postEvent('web_app_setup_back_button', { is_visible: true })
+    // postEvent('web_app_setup_back_button', { is_visible: true })
   })
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-Test Event
+<p style="margin-top: 2rem; font-weight: 700;">Console</p>
+<p bind:this={consEl}></p>
+
+<style lang="scss">
+  .a {
+    font-weight: 500;
+  }
+</style>
