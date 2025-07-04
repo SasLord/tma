b7c88456a9cb1a0c83871275b4567dc160be9b7e
Alexander Platov <alexander.platov@hardcoreeng.com>
2025-07-05 01:53:28 +0700

    add telegram-sdk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import { init, miniApp } from '@telegram-apps/sdk-svelte'
+
+  const initializeTelegramSDK = async () => {
+    try {
+      await init()
+
+
+      if (miniApp.ready.isAvailable()) {
+        await miniApp.ready()
+        console.log('Mini App готово')
+      }
+
+
+    } catch (error) {
+      console.error('Ошибка инициализации:', error)
+    }
+  }
+
+  onMount(() => {
+    void initializeTelegramSDK()
+    const data = JSON.stringify({
+      eventType: 'web_app_setup_back_button',
+      eventData: {
+        is_visible: true
+      }
+    })
+
+    window.parent.postMessage(data, '*')
+  })
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
